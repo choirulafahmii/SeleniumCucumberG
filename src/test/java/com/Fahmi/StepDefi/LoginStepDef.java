d6ee4967123fac7ff81853ddc8fa0e1d4876cba6
choirulafahmii <choirulafahmi.work@gmail.com>
2025-04-24 18:24:07 +0700

    updated

--- src/test/java/com/Fahmi/StepDefi/LoginStepDef.java
+++ src/test/java/com/Fahmi/StepDefi/LoginStepDef.java
@@ -7,9 +7,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.sql.Driver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginStepDef {
 
@@ -22,7 +27,7 @@ public class LoginStepDef {
 
     @After
     public void afterTest(){
-
+        driver.close();
     }
 
     @Given("user is on login page")
@@ -32,25 +37,33 @@ public class LoginStepDef {
 
     @And("user input username with {string}")
     public void userInputUsernameWith(String username) {
-
+        By usernameInputText = By.cssSelector("input#user-name.form_input");
+        driver.findElement(usernameInputText).sendKeys(username);
     }
 
     @And("user input password with {string}")
     public void userInputPasswordWith(String password) {
-        
+        By passwordInputText = By.xpath("//*[@id=\"password\"]");
+        driver.findElement(passwordInputText).sendKeys(password);
     }
 
     @When("user click login button")
     public void userClickLoginButton() {
-        
+        By loginButton = By.id("login-button");
+        driver.findElement(loginButton).click();
     }
 
     @Then("user is on homepage")
     public void userIsOnHomepage() {
+        By productTitle = By.xpath("//*[@id=\"item_4_title_link\"]");
+        WebElement productElement = driver.findElement(productTitle);
+        assertTrue(productElement.isDisplayed());
+        assertEquals("Sauce Labs Backpack", productElement.getText());
+
     }
 
-
-    @And("user see error message")
-    public void userSeeErrorMessage() {
+    @Then("user see error message {string}")
+    public void userSeeErrorMessage(String errorMessage) {
+        assertTrue(driver.getPageSource().contains(errorMessage));
     }
 }
--- src/test/resources/login2.feature
+++ src/test/resources/login2.feature
@@ -1,14 +1,5 @@
 Feature: Login
 
-  @invalid-login
-  Scenario: Login using invalid username and password
-    Given user is on login page
-    And user input username with "standard_user"
-    And user input password with "secret_sauce"
-    When user click login button
-    Then user is on homepage
-    And user see error message
-
   @valid-login
   Scenario: Login using valid username and password
     Given user is on login page
@@ -16,3 +7,13 @@ Feature: Login
     And user input password with "secret_sauce"
     When user click login button
     Then user is on homepage
+
+  @invalid-login
+  Scenario: Login using invalid username and password
+    Given user is on login page
+    And user input username with "standard_user"
+    And user input password with "secret_saue"
+    When user click login button
+    Then user is on homepage
+    Then user see error message "Epic sad face: Username and password do not match any user in this service"
+
